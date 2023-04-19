df4f6ca5ce5eeefadede651f5a4a5e814008c523
Wilfried Beslin <wbeslin@gmail.com>
2023-04-19 15:35:19 -0300

    Remove version numbers from module directory names and revise version number info in master scripts

---
+++ _BWD/BWD_master.m
@@ -0,0 +1,64 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%
+% "BWD_master"
+%   Written by Wilfried Beslin
+%   For TWD version 1.3
+%   Revised 2023/04/19
+%
+%   Despcription:
+%   Interface script for running WB's version of the beaked whale detector.
+%   Edit the variables in this file as needed, then run to launch automatic 
+%   beaked whale detection.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% CHANGE AS NEEDED %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% deployment name
+depName = 'TEST';
+
+% path to analysis folder
+dirPath_analysis = 'C:\Users\BeslinW\Desktop\Beakies\TWD_Testing\Testing_BWD_1month'; 
+
+% path to WAV file folder
+%%% For step 1: click compilation. Set empty to ignore this step.
+dirPath_audio = 'C:\Users\BeslinW\Desktop\Beakies\TWD_Testing\audio_1month';
+
+% name of output folder for event detection results
+%%% For step 2: beaked whale detection. Set empty to ignore this step.
+dirName_detResults = 'results02';
+
+% name of detection protocol
+%%% Used in Step 2 only
+detProtocol = 'GeneralWithDiscriminators';
+
+% data segmentation 
+%%% best for continuous data. Set to duration(Inf,Inf,Inf) for duty-cycled.
+%%% Must be a MATLAB duration object. Syntax is "duration(h,m,s)".
+segDur = duration(Inf,Inf,Inf);
+
+% FFT samples
+nfft = 512;  
+
+% Max clicks per MAT file
+nMATClicksMax = 10000; 
+
+% END CHANGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% determine path to code directory
+dirPath_root = mfilename('fullpath');
+[dirPath_root,~,~] = fileparts(dirPath_root);
+
+% run detector
+BWD.run(...
+    dirPath_root,...
+    depName,...
+    dirPath_analysis,...
+    dirPath_audio,...
+    dirName_detResults,...
+    detProtocol,...
+    segDur,...
+    nfft,...
+    nMATClicksMax);
---
+++ _SWD/SWDValidate_master.m
@@ -0,0 +1,55 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%
+% "SWDValidate_master"
+%   Written by Wilfried Beslin
+%   For TWD version 1.3
+%   Revised 2023/04/19
+%
+%   Despcription:
+%   Interface script for running interactive sperm whale click 
+%   validation/species identification procedure. Edit the variables in this 
+%   file as needed, then run to launch the species ID app.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% CHANGE AS NEEDED %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% path to analysis folder
+dirPath_analysis = 'C:\Users\BeslinW\Desktop\Beakies\TWD_Testing\Testing_SWD_1-3';
+
+% name of results folder
+dirName_detResults = 'results03';
+
+% event merging option
+%%% only used once, i.e. if validation spreadsheet doesn't exist yet
+eventMergeOpt = 'none'; %%% Options are 'none', 'timegap', 'calendar', or 'timebin'
+eventMergeVal = ''; %%% MATLAB duration object (for timegap and timebin) or string (for calendar)
+
+% max number of clicks to display
+nClicksMax = 10000;
+
+% default frequency display range
+fRange = [0, 50];
+
+% ID of monitor on which to show figures
+%%% as shown in Windows "Display settings"
+monID = 2;
+
+% END CHANGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% determine path to code directory
+dirPath_root = mfilename('fullpath');
+[dirPath_root,~,~] = fileparts(dirPath_root);
+
+% run process
+SWD.validate(...
+    dirPath_root,...
+    dirPath_analysis,...
+    dirName_detResults,...
+    eventMergeOpt,...
+    eventMergeVal,...
+    nClicksMax,...
+    fRange,...
+    monID);
